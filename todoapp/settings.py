9759ae05506af1ac8e129a621e73f9ed47cca4d5
Diaz Harizky Firdaus <diazharizky@gmail.com>
2023-09-13 11:58:13 +0700

    Utilize docker

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = "todoapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todoapp",
+        "USER": "todoapp",
+        "PASSWORD": "todoapp",
+        "HOST": "localhost",
+        "PORT": "5432",
+    },
 }
 
 
